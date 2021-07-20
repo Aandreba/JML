da787f1da42c96f1ed43d9d7b43670b41c9bc8be
Yuquio <info@yuquio.com>
2021-07-21 01:54:34 +0200

    Fourier Series support for Bezier curves

--- src/org/jml/Mathx/FourierSeries.java
+++ src/org/jml/Mathx/FourierSeries.java
@@ -3,6 +3,7 @@ package org.jml.Mathx;
 import org.jml.Calculus.Integral;
 import org.jml.Complex.Single.Comp;
 import org.jml.Matrix.Single.Mati;
+import org.jml.Vector.Single.Vec;
 import org.jml.Vector.Single.Veci;
 
 import java.awt.*;
@@ -11,6 +12,8 @@ import java.awt.geom.Path2D;
 import java.awt.geom.PathIterator;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
 
 public class FourierSeries {
     public static Veci calculate (int length, Integral.ComplexFunction function) {
@@ -21,25 +24,7 @@ public class FourierSeries {
     }
 
     public static Veci calculate (int length, PathIterator path) {
-        ArrayList<Comp> points = new ArrayList<>();
-        float[] point = new float[2];
-
-        while (!path.isDone()) {
-            path.currentSegment(point);
-            path.next();
-            points.add(new Comp(point[0], point[1]));
-        }
-
-        int k = points.size() - 1;
-        Integral.ComplexFunction func = (float t) -> {
-            float n = points.size() * t;
-            int from = (int) Mathf.clamp(Mathf.floor(n), 0, k);
-            int to = (int) Mathf.clamp(Mathf.ceil(n), 0, k);
-
-            return points.get(from).mul(n - from).add(points.get(to).mul(to - n));
-        };
-
-        return calculate(length, func);
+        return calculate(length, pathToFunction(path));
     }
 
     public static Comp compute (float time, Veci rotators) {
@@ -52,5 +37,69 @@ public class FourierSeries {
         }
 
         return sum;
+    }
+
+    public static Integral.ComplexFunction pathToFunction (PathIterator path) {
+        ArrayList<float[]> points = new ArrayList<>();
+        ArrayList<Integer> types = new ArrayList<>();
+
+        float[] point = new float[6];
+        while (!path.isDone()) {
+            int type = path.currentSegment(point);
+            path.next();
+
+            points.add(point);
+            types.add(type);
+        }
+
+        float[] fst = points.get(0);
+        Comp first = new Comp(fst[0], fst[1]);
+
+        return (float t) -> {
+            float j = (points.size() * t);
+            int n = (int) j; // To
+            if (n == 0) {
+                return first;
+            }
+
+            int k = n - 1; // From
+            float _t = j - n;
+            float _1mt = 1 - _t;
+
+            float[] target = points.get(n);
+            float[] prev = points.get(k);
+
+            Comp lastPos = new Comp(prev[prev.length-2], prev[prev.length-1]);
+            int type = types.get(n);
+
+            return switch (type) {
+                case PathIterator.SEG_CUBICTO -> {
+                    float _1mt_2 = _1mt * _1mt;
+                    float _t2 = _t * _t;
+
+                    Comp alpha = new Comp(target[0], target[1]);
+                    Comp beta = new Comp(target[2], target[3]);
+                    Comp gamma = new Comp(target[4], target[5]);
+
+                    yield lastPos.mul(_1mt_2 * _1mt)
+                            .add(alpha.mul(3 * _1mt_2 * _t))
+                            .add(beta.mul(3 * _1mt * _t2))
+                            .add(gamma.mul(_t2 * _t));
+
+                }
+
+                case PathIterator.SEG_QUADTO -> {
+                    Comp alpha = new Comp(target[0], target[1]);
+                    Comp beta = new Comp(target[2], target[3]);
+
+                    yield lastPos.mul(_1mt * _1mt)
+                            .add(alpha.mul(2 * _1mt))
+                            .add(beta.mul(_t * _t));
+                }
+
+                case PathIterator.SEG_MOVETO -> new Comp(target[0], target[1]);
+                default -> lastPos.mul(j-k).add(new Comp(target[0], target[1]).mul(n-j));
+            };
+        };
     }
 }
--- test/Main.java
+++ test/Main.java
@@ -1,3 +1,4 @@
+import org.jml.Calculus.Integral;
 import org.jml.Complex.Single.Comp;
 import org.jml.Mathx.FourierSeries;
 import org.jml.Mathx.Mathf;
@@ -10,13 +11,16 @@ public class Main {
     public static void main(String... args) {
         Path2D.Float triangle = new Path2D.Float();
         triangle.moveTo(-1, -1);
-        triangle.lineTo(0, 1);
-        triangle.lineTo(1, -1);
-        triangle.lineTo(-1, -1);
+        triangle.curveTo(-1, -1, 1, 1, -1, 1);
 
-        Veci series = FourierSeries.calculate(10, triangle.getPathIterator(null));
+        Integral.ComplexFunction func = FourierSeries.pathToFunction(triangle.getPathIterator(null));
+        for (float t=0;t<=1;t+=0.01f) {
+            System.out.println(t+": "+func.apply(t).real);
+        }
+
+        /*Veci series = FourierSeries.calculate(10, triangle.getPathIterator(null));
         for (float t=0;t<=1;t+=0.01f) {
             System.out.println(t+": "+FourierSeries.compute(t, series));
-        }
+        }*/
     }
 }
